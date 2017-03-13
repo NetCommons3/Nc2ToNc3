ae99e70d27275050d3616232ec9f0c7a0ee00deb
kteraguchi <kteraguchi@commonsnet.org>
2017-03-13 14:37:32 +0900

    Add Nc2ToNc3BaseBehavior::convertTitleIcon

--- Model/Behavior/Nc2ToNc3BaseBehavior.php
+++ Model/Behavior/Nc2ToNc3BaseBehavior.php
@@ -196,6 +196,17 @@ class Nc2ToNc3BaseBehavior extends ModelBehavior {
 	}
 
 /**
+ * Convert nc2 title_icon.
+ *
+ * @param Model $model Model using this behavior.
+ * @param string $titleIcon Nc2 title_icon.
+ * @return string converted nc3 title_icon.
+ */
+	public function convertTitleIcon(Model $model, $titleIcon) {
+		return $this->_convertTitleIcon($titleIcon);
+	}
+
+/**
  * Write migration log.
  *
  * @param string $message Migration message.
@@ -440,6 +451,125 @@ class Nc2ToNc3BaseBehavior extends ModelBehavior {
 		}
 
 		return $nc2Value;
+	}
+
+/**
+ * Convert nc2 title_icon.
+ *
+ * @param string $titleIcon Nc2 title_icon.
+ * @return string converted nc3 title_icon.
+ * @SuppressWarnings(PHPMD.ExcessiveMethodLength)
+ */
+	protected function _convertTitleIcon($titleIcon) {
+		$map = [
+			'smiley/smiley-smile1.gif' => '40_010_smile.svg',
+			'smiley/smiley-smile2.gif' => '40_011_laugh.svg',
+			'smiley/smiley-smile3.gif' => '40_010_smile.svg',
+			'smiley/smiley-smile4.gif' => '40_012_expect.svg',
+			'smiley/smiley-smile5.gif' => '40_011_laugh.svg',
+			'smiley/smiley-smile6.gif' => '40_011_laugh.svg',
+			'smiley/smiley-smile7.gif' => '40_013_pleased.svg',
+			'smiley/smiley-smile8.gif' => '40_013_pleased.svg',
+			'smiley/smiley-smile9.gif' => '40_011_laugh.svg',
+			'smiley/smiley-smile10.gif' => '40_014_joke.svg',
+			'smiley/smiley-smile11.gif' => '40_015_excited.svg',
+			'smiley/smiley-smile12.gif' => '40_015_excited.svg',
+			'smiley/smiley-cryhappily1.gif' => '40_030_cried_for_joy.svg',
+			'smiley/smiley-sweat1.gif' => '40_020_bewilderment.svg',
+			'smiley/smiley-sweat2.gif' => '40_020_bewilderment.svg',
+			'smiley/smiley-sweat3.gif' => '40_021_worry.svg',
+			'smiley/smiley-sweat4.gif' => '40_022_trouble.svg',
+			'smiley/smiley-forcedsmile1.gif' => '40_023_bitter_smile.svg',
+			'smiley/smiley-forcedsmile2.gif' => '40_023_bitter_smile.svg',
+			'smiley/smiley-forcedsmile3.gif' => '40_023_bitter_smile.svg',
+			'smiley/smiley-despair1.gif' => '40_024_shock.svg',
+			'smiley/smiley-cry1.gif' => '40_026_patience.svg',
+			'smiley/smiley-cry2.gif' => '40_030_cry.svg',
+			'smiley/smiley-cry3.gif' => '40_030_cry.svg',
+			'smiley/smiley-cry4.gif' => '40_030_cried_for_joy.svg',
+			'smiley/smiley-cry5.gif' => '40_030_cry.svg',
+			'smiley/smiley-cry6.gif' => '40_030_cry.svg',
+			'smiley/smiley-thanks1.gif' => '40_040_bow.svg',
+			'smiley/smiley-hmm1.gif' => '40_010_smile.svg',
+			'smiley/smiley-grin1.gif' => '40_016_gloat.svg',
+			'smiley/smiley-anger1.gif' => '40_050_angry.svg',
+			'smiley/smiley-anger2.gif' => '40_050_angry.svg',
+			'smiley/smiley-lookconfused1.gif' => '40_010_smile.svg',
+			'smiley/smiley-other1.gif' => '40_061_glasses.svg',
+			'smiley/smiley-other2.gif' => '40_061_glasses.svg',
+			'smiley/smiley-other3.gif' => '40_010_smile.svg',
+			'smiley/smiley-other4.gif' => '40_010_smile.svg',
+			'smiley/smiley-other5.gif' => '40_010_smile.svg',
+			'smiley/smiley-other6.gif' => '40_010_smile.svg',
+			'titleicon/icon-new.gif' => '10_010_new.svg',
+			'titleicon/icon-weather1.gif' => '20_010_sunny.svg',
+			'titleicon/icon-weather2.gif' => '20_011_cloudy.svg',
+			'titleicon/icon-weather3.gif' => '20_012_party_cloudy.svg',
+			'titleicon/icon-weather4.gif' => '20_013_rainy.svg',
+			'titleicon/icon-weather5.gif' => '20_014_light_rain.svg',
+			'titleicon/icon-weather6.gif' => '20_015_heavy_rain.svg',
+			'titleicon/icon-weather7.gif' => '20_016_storm.svg',
+			'titleicon/icon-weather8.gif' => '20_017_thunders.svg',
+			'titleicon/icon-weather9.gif' => '20_018_snow.svg',
+			'titleicon/icon-star1.gif' => '20_019_star.svg',
+			'titleicon/icon-star2.gif' => '20_019_star.svg',
+			'titleicon/icon-star3.gif' => '10_060_one_star.svg',
+			'titleicon/icon-work.gif' => '30_040_business.svg',
+			'titleicon/icon-businesstrip.gif' => '30_020_trip.svg',
+			'titleicon/icon-train.gif' => '30_021_train.svg',
+			'titleicon/icon-car.gif' => '30_022_car.svg',
+			'titleicon/icon-important.gif' => '10_020_exclamation.svg',
+			'titleicon/icon-deadline.gif' => '10_023_deadline.svg',
+			'titleicon/icon-haste.gif' => '10_080_rush.svg',
+			'titleicon/icon-announcement.gif' => '10_030_announce.svg',
+			'titleicon/icon-ok1.gif' => '10_064_hanamaru.svg',
+			'titleicon/icon-ok2.gif' => '10_072_roger.svg',
+			'titleicon/icon-ok3.gif' => '10_011_ok.svg',
+			'titleicon/icon-arrow1.gif' => '10_040_left.svg',
+			'titleicon/icon-arrow2.gif' => '10_041_right.svg',
+			'titleicon/icon-question.gif' => '10_022_question.svg',
+			'titleicon/icon-headcount1.gif' => '40_010_smile.svg',
+			'titleicon/icon-headcount2.gif' => '30_060_group.svg',
+			'titleicon/icon-headcount3.gif' => '30_060_group.svg',
+			'titleicon/icon-headcount4.gif' => '30_060_group.svg',
+			'titleicon/icon-meeting.gif' => '30_041_meeting.svg',
+			'titleicon/icon-flash.gif' => '30_061_light.svg',
+			'titleicon/icon-sport.gif' => '30_011_sport.svg',
+			'titleicon/icon-science.gif' => '30_010_science.svg',
+			'titleicon/icon-music.gif' => '30_014_music.svg',
+			'titleicon/icon-pc.gif' => '30_015_computer.svg',
+			'titleicon/icon-meal.gif' => '30_017_lunch.svg',
+			'titleicon/icon-movie.gif' => '30_016_audiovisual.svg',
+			'titleicon/icon-anniversary.gif' => '30_031_anniversary.svg',
+			'titleicon/icon-heart.gif' => '10_080_heart.svg',
+			'titleicon/icon-excursion.gif' => '30_033_picnic.svg',
+			'titleicon/icon-teabreak.gif' => '30_062_coffee.svg',
+			'titleicon/icon-animal1.gif' => '10_081_pow.svg',
+			'titleicon/icon-animal2.gif' => '10_081_pow.svg',
+			'titleicon/icon-true.gif' => '10_044_circle.svg',
+			'titleicon/icon-false.gif' => '10_045_cross.svg',
+			'titleicon/icon-telephone.gif' => '30_050_mobile.svg',
+			'titleicon/icon-brilliant.gif' => '10_082_glitter.svg',
+			'titleicon/icon-bus.gif' => '30_024_bus.svg',
+			'titleicon/icon-ambulance.gif' => '30_062_hospital.svg',
+			'titleicon/icon-school.gif' => '30_063_school.svg',
+			'titleicon/icon-hospital.gif' => '30_062_hospital.svg',
+			'titleicon/icon-emergency.gif' => '30_062_hospital.svg',
+			'titleicon/icon-note.gif' => '30_052_note.svg',
+			'titleicon/icon-pencil.gif' => '30_053_pencil.svg',
+			'titleicon/icon-magnifier.gif' => '30_055_search.svg',
+			'titleicon/icon-clammyskin.gif' => '10_083_impatient.svg',
+			'titleicon/icon-morning.gif' => '30_070_morning.svg',
+			'titleicon/icon-day.gif' => '30_071_afternoon.svg',
+			'titleicon/icon-evening.gif' => '30_073_evening.svg',
+			'titleicon/icon-night.gif' => '30_073_evening.svg',
+			'titleicon/icon-mail.gif' => '30_054_email.svg',
+		];
+
+		// @see https://github.com/NetCommons3/NetCommons/blob/3.1.0/View/Helper/TitleIconHelper.php#L211
+		$titleIcon = '/net_commons/img/title_icon/' . Hash::get($map, [$titleIcon]);
+
+		return $titleIcon;
 	}
 
 }
