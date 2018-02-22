137a4030e78927996d803d72134c2364a7d8d544
Mitsuru Mutaguchi <akagane99@gmail.com>
2018-02-22 19:43:11 +0900

    test: add Alltest

---
+++ Test/Case/AllNc2ToNc3Test.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Nc2ToNc3 All Test Case
+ *
+ * @author Mitsuru Mutaguchi <mutaguchi@opensource-workshop.jp>
+ * @link http://www.netcommons.org NetCommons Project
+ * @license http://www.netcommons.org/license.txt NetCommons License
+ * @copyright Copyright 2014, NetCommons Project
+ */
+
+App::uses('NetCommonsTestSuite', 'NetCommons.TestSuite');
+
+/**
+ * Nc2ToNc3 All Test Case
+ *
+ * @author Mitsuru Mutaguchi <mutaguchi@opensource-workshop.jp>
+ * @package NetCommons\Nc2ToNc3\Test\Case
+ * @codeCoverageIgnore
+ */
+class AllNc2ToNc3Test extends CakeTestSuite {
+
+/**
+ * All test suite
+ *
+ * @return CakeTestSuite
+ */
+	public static function suite() {
+		$plugin = preg_replace('/^All([\w]+)Test$/', '$1', __CLASS__);
+		$suite = new NetCommonsTestSuite(sprintf('All %s Plugin tests', $plugin));
+		$suite->addTestDirectoryRecursive(CakePlugin::path($plugin) . 'Test' . DS . 'Case');
+		return $suite;
+	}
+}
